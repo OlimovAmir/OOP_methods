83c0cf4756a47ebb66f7d0dfef6586a0ca2e4508
olimov Amir <olimov.amir@mail.ru>
2023-12-16 12:41:13 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using OOP_methods;
+
+class Program
+{
+    static Student GetStudent()
+    {
+        OOP_methods.Student student = new OOP_methods.Student();
+
+
+        student.lastName = "Amir";
+        student.firstName = "Olimov";
+        student.middleName = "Muhamadaminovich";
+        student.age = 36;
+        student.Id = Guid.NewGuid();
+        student.group = Group.IT;
+
+        return student;
+    }
+
+    //OOP_methods.Student student1 = new OOP_methods.Student();
+    static void Main(string[] args)
+    {
+        var student = GetStudent();
+
+        student.Print();
+
+        student.GetFullName();
+        
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_methods
+{
+    public enum Group
+    {
+        fizik,
+        Matematic,
+        Himik,
+        IT
+    }
+    public class Student
+    {
+        public Guid Id;
+        public string firstName;
+        public string lastName;
+        public string middleName;
+        public int age;
+        public Group group;
+
+        public void Print()
+        {
+            Console.WriteLine("info obout Student");
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"lastName: {lastName}");
+            Console.WriteLine($"firstName: {firstName}");
+            Console.WriteLine($"middleName: {middleName}");
+            Console.WriteLine($"age: {age}");
+            Console.WriteLine($"group: {group}");
+        }
+        
+        public string GetFullName()
+        {
+            return $"{firstName} {lastName} {middleName}";
+        }
+
+    }
+}
